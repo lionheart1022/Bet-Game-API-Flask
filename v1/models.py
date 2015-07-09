5e177fa7f2ab0fa10bf4038bf86ab5acdd54a583
Semyon Maryasin <simeon@maryasin.name>
2015-07-09 18:08:18 +0300

    Added basic models

--- v1/models.py
+++ v1/models.py
@@ -7,6 +7,37 @@ import sqlalchemy
 from .main import db
 import config
 
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_nick = db.Column(db.String(64), required=True)
+    email = db.Column(db.String(128), required=True)
+    password = db.Column(db.LargeBinary(36))
+    facebook_token = db.Column(db.String(128))
+    create_date = db.Column(db.DateTime, default=datetime.utcnow)
+    games = db.relationship('Game')
+    balance = db.Column(db.Float, default=0)
+
+
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), index=True)
+    player = db.relationship(Player, backref='devices')
+    push_token = db.Column(db.String(128), required=True)
+    last_login = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey('player.id'), index=True)
+    creator = db.relationship(Player, backref='games_created')
+    opponent_id = db.Column(db.Integer, db.ForeignKey('player.id'), index=True)
+    opponent = db.relationship(Player, backref='games_invited')
+    bet = db.Column(db.Float, required=True)
+    create_date = db.Column(db.DateTime, default=datetime.utcnow)
+    state = db.Column(db.Enum('new', 'accepted', 'declined'), default='new')
+    accept_date = db.Column(db.DateTime, nullable=True)
+
+
 def fast_count(query):
     """
     Get count of queried items avoiding using subquery (like query.count() does)
