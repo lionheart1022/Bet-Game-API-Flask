5e1c0fb60243491ffb83b4cec7f78f09ddae5dc6
Semyon Maryasin <simeon@maryasin.name>
2015-07-21 22:48:36 +0300

    avoid div error in gametype_image

--- v1/routes.py
+++ v1/routes.py
@@ -427,7 +427,7 @@ def gametype_image(id):
     img = Image.open('images/{}.png'.format(id))
     ow, oh = img.size
     if args.w or args.h:
-        if not args.h or (args.w/args.h) > (ow/oh):
+        if not args.h or (args.w and args.h and (args.w/args.h) > (ow/oh)):
             dw = args.w
             dh = round(oh / ow * dw)
         else:
