91bfd25fe21b5b7158dfebd6ec378b5bdb5bad88
Semyon Maryasin <simeon@maryasin.name>
2015-08-19 16:32:05 +0300

    Defined api

--- observer.py
+++ observer.py
@@ -4,6 +4,8 @@
 # listens on some port/path,
 # may have some children configured,
 # knows how many streams can it handle.
+# Also each host accepts connections only from its known siblings.
+#
 # Messaging flow:
 #
 # Client -> Master: Please watch the stream with URL ... for game ...
@@ -12,6 +14,12 @@
 # Slave1 -> Master: no
 # Master -> Slave2: Can you watch one more stream? (details)
 # Slave2 -> Master: yes
+# (or if none agreed - tries to watch itself)
 # ...
 # Slave2 -> Master: stream X finished, result is Xres
 # Master -> Poller: stream X done
+
+# API:
+# PUT /streams/id - watch the stream (client->master->slave)
+# PATCH /streams/id - stream done (slave->master)
+
