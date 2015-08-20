bd219dd8eed7f8dd119a197a59c9d7908c075bcf
Semyon Maryasin <simeon@maryasin.name>
2015-08-20 15:22:05 +0300

    fixed source directive

--- observer.py
+++ observer.py
@@ -188,7 +188,7 @@ class Handler:
             os.chdir(cls.path)
         cmd = cls.process.format(handle = stream.handle)
         if cls.env:
-            cmd = 'source {}/bin/activate; {}'.format(cls.env, cmd)
+            cmd = '. {}/bin/activate; {}'.format(cls.env, cmd)
         sub = subprocess.Popen(
             cmd,
             bufsize = 1, # line buffered
