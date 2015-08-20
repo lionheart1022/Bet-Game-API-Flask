55eb14a85eea8f1554d2ffad67997eb6530895b8
Semyon Maryasin <simeon@maryasin.name>
2015-08-20 15:05:09 +0300

    use reloader

--- observer.py
+++ observer.py
@@ -504,4 +504,4 @@ def load_ep():
 
 if __name__ == '__main__':
     init_app()
-    app.run(port=8021, debug=False, use_debugger=False)
+    app.run(port=8021, debug=False, use_debugger=False, use_reloader=True)
