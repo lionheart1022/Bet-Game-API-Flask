cb07ba451f802145b682665cf6f9a500a65d7ec4
Semyon Maryasin <simeon@maryasin.name>
2015-08-21 11:55:57 +0300

    Observer: changed logs storage

--- observer.sh
+++ observer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 exec gunicorn "$@" \
-	--access-logfile /home/betgame/observer-access.log \
-	--error-logfile /home/betgame/observer-errors.log \
 	--workers 1 --worker-class eventlet \
 	--bind localhost:8021 -m 007 \
-	'observer:init_app("/home/betgame/observer.log")'
+	'observer:init_app()' \
+	&>> /home/betgame/observer.log
