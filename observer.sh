4678ce8c691731aeba0dc682db7744093cc32049
Semyon Maryasin <simeon@maryasin.name>
2015-08-21 11:52:15 +0300

    observer.sh: use logfiles

--- observer.sh
+++ observer.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-exec gunicorn "$@" --workers 1 --worker-class eventlet --bind localhost:8021 -m 007 'observer:init_app("/home/betgame/observer.log")'
+exec gunicorn "$@" \
+	--access-logfile /home/betgame/observer-access.log \
+	--error-logfile /home/betgame/observer-errors.log \
+	--workers 1 --worker-class eventlet \
+	--bind localhost:8021 -m 007 \
+	'observer:init_app("/home/betgame/observer.log")'
