03b9e295a779367fcde5cadb20de965a4c39a6a8
Semyon Maryasin <simeon@maryasin.name>
2015-08-22 21:26:59 +0300

    Added gametype caching loader

--- site.py
+++ site.py
@@ -12,6 +12,25 @@ app.config['SECRET_KEY'] = config.JWT_SECRET
 app.config['API'] = 'http://betgame.co.uk/v1'
 app.config['API_ROOT'] = 'http://betgame.co.uk/v1'
 
+class Gametypes:
+    """
+    Caching access to /gametypes endpoint
+    """
+    cache = None
+    @classmethod
+    def _load(cls):
+        # TODO: expiration timeouts
+        if not cls.cache:
+            cls.cache = requests.get(app.config['API_ROOT']+'/gametypes').json()
+            cls.dcache = {x['id']: x for x in cls.cache['gametypes']}
+        return cls.dcache
+
+    @classmethod
+    def get(cls, name=None):
+        if not name:
+            return cls._load().values()
+        return cls._load().get(name)
+
 @app.route('/')
 def bets():
     return render_template('newbet.html')
@@ -25,12 +44,14 @@ def gametype():
     ret = requests.get(app.config['API_ROOT']+'/gametypes').json()
     games = ret.get('gametypes')
     modes = None
+    print('gt: '+str(session.get('gametype', 'no')))
     if session.get('gametype'):
         game = next(filter(
             lambda g: g['id'] == session['gametype'],
             games,
         ), None)
         modes = game['gamemodes']
+        print('t: %s, m:%s' % (game, modes))
     return render_template('gametype.html', games=games, gamemodes=modes)
 
 @app.route('/leaders')
