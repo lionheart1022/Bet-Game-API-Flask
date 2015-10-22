499e224952ad8d32a6d0630dd51c4c3f2b35b09a
Semyon Maryasin <simeon@maryasin.name>
2015-10-22 20:00:41 +0300

    config: added twitter key/secret

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from secret import DB_URL, JWT_SECRET
 #from secret import GOOGLE_AUTH_API_KEY
 #from secret import GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_CLIENT_SECRET
 from secret import FACEBOOK_AUTH_CLIENT_ID, FACEBOOK_AUTH_CLIENT_SECRET
+from secret import TWITTER_API_KEY, TWITTER_API_SECRET
 #from secret import ADMIN_USERS
 from secret import PAYPAL_CLIENT, PAYPAL_SECRET, PAYPAL_SANDBOX
 from secret import MAILGUN_KEY
