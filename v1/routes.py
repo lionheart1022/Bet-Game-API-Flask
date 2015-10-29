f01173949a21a88660af500a48bb81bc950ad910
Semyon Maryasin <simeon@maryasin.name>
2015-10-29 07:16:21 +0300

    str

--- v1/routes.py
+++ v1/routes.py
@@ -175,7 +175,7 @@ class PlayerResource(restful.Resource):
                 # ...and always add player.id to stabilize order
             if not args.order or not args.order.endswith('id'):
                 orders.append(Player.id)
-            log.debug('ordering: '+repr(orders))
+            log.debug('ordering: '+str(orders))
             if args.order:
                 orders = map(
                     operator.methodcaller(
