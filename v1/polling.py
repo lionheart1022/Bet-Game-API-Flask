8f86aa248c9faf2c906a9484e1406675cf28f931
Semyon Maryasin <simeon@maryasin.name>
2015-12-09 06:54:24 +0300

    Fixed poller preparing

--- v1/polling.py
+++ v1/polling.py
@@ -1077,6 +1077,6 @@ if __name__ == '__main__':
     poller.gameStarted(Game.query.first()) # FIXME
 
     pin = poller()
-    if args.gamemode:
+    if args.gamemode or not poller.usemodes:
         pin.prepare()
     pin.poll(args.now, args.gametype, args.gamemode)
