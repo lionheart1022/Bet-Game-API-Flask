ca392a04c54ea06b49574641a69949af31c58ada
Semyon Maryasin <simeon@maryasin.name>
2015-12-27 02:44:37 +0300

    added 3000 localhost to cors

--- config.py
+++ config.py
@@ -46,6 +46,8 @@ CORS_ORIGINS = [
     'http://dev.betgame.co.uk',
     'http://127.0.0.1:8080',
     'http://localhost:8080',
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
 if 'test' in __file__:
     CORS_ORIGINS.append('http://test.betgame.co.uk')
