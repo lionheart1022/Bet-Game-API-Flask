b3d8e2d47aaf83286f363c44c12a9162a2f5c03c
Semyon Maryasin <simeon@maryasin.name>
2015-12-28 02:28:29 +0300

    Added flask-redis

--- v1/main.py
+++ v1/main.py
@@ -2,11 +2,13 @@ from flask import Blueprint
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext import restful
 from flask.ext.socketio import SocketIO
+from flask.ext.redis import FlaskRedis
 
 app = Blueprint('v1', __name__)
 db = SQLAlchemy()
 api = restful.Api(prefix='/v1')
 socketio = SocketIO()
+redis = FlaskRedis()
 
 _before1req = []
 def before_first_request(func):
@@ -16,6 +18,7 @@ def init_app(flask_app):
     db.init_app(flask_app)
     api.init_app(flask_app)
     socketio.init_app(flask_app)
+    redis.init_app(flask_app)
     flask_app.register_blueprint(app, url_prefix='/v1')
     flask_app.before_first_request_funcs.extend(_before1req)
 
